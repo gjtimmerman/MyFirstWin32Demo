5fe0dbf2c47715373751f05ec25764b85ca3e124
Gert Jan Timmerman <gertt@infosupport.com>
2021-06-08 12:51:45 +0200

    Window with Rectangle

---
+++ MyFirstWin32Demo/MyFirstWin32Demo.c
@@ -0,0 +1,54 @@
+#include <windows.h>
+#include <tchar.h>
+#include "resource.h"
+
+
+HINSTANCE hInst;
+HWND hWndMain;
+LRESULT MyWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+		PAINTSTRUCT ps;
+	case WM_PAINT:
+		BeginPaint(hWnd, &ps);
+		Rectangle(ps.hdc, 10, 10, 100, 100);
+		EndPaint(hWnd,&ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR cmdLine, int cmdShow)
+{
+	hInst = hInstance;
+	WNDCLASSEX wc;
+	memset(&wc, 0, sizeof(WNDCLASSEX));
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.lpfnWndProc = MyWndProc;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon((HINSTANCE)NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor((HINSTANCE)NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.lpszMenuName = MAKEINTRESOURCE(IDR_MENU1);
+	wc.lpszClassName = _T("MyMainWndClass");
+	if (!RegisterClassEx(&wc))
+	{
+		DWORD err = GetLastError();
+		return 0;
+	}
+
+	hWndMain = CreateWindow(_T("MyMainWndClass"), _T("MyWindow"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, (HWND)NULL, (HMENU)NULL, hInst, (LPVOID)NULL);
+	ShowWindow(hWndMain, SW_SHOWNORMAL);
+	UpdateWindow(hWndMain);
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
