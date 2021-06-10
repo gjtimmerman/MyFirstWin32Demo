6d241cbe6248a3d962f9e0e3014c842d4fab1fb9
Gert Jan Timmerman <gertt@infosupport.com>
2021-06-10 13:53:37 +0200

    Dialog Box

--- MyFirstWin32Demo/MyFirstWin32Demo.c
+++ MyFirstWin32Demo/MyFirstWin32Demo.c
@@ -5,6 +5,28 @@
 
 HINSTANCE hInst;
 HWND hWndMain;
+
+INT_PTR MyDlgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	int cmd = LOWORD(wParam);
+	switch (msg)
+	{
+	case WM_COMMAND:
+		if (cmd == IDC_BUTTON1)
+		{
+			TCHAR buffer[80];
+			GetDlgItemText(hWnd, IDC_EDIT1, buffer, 80);
+			MessageBox(hWnd, buffer, _T("Dit is de Tekst"), MB_OK);
+			return TRUE;
+		}
+		else if (cmd == IDC_BUTTON2)
+			EndDialog(hWnd, IDOK);
+		break;
+	}
+	return FALSE;
+}
+
+
 RECT myRect = { 10,10,100,100 };
 LRESULT MyWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -55,12 +77,17 @@ LRESULT MyWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 					DWORD x = error;
 				}
 			}
+			else if (item == ID_HELP_ABOUT)
+			{
+				DialogBox(NULL, MAKEINTRESOURCE(IDD_FORMVIEW), hWnd, MyDlgProc);
+			}
 		}
 		break;
 	}
 	}
 	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
+
 
 int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR cmdLine, int cmdShow)
 {
@@ -75,13 +102,14 @@ int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR cmdLine, i
 	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
 	wc.lpszMenuName = MAKEINTRESOURCE(IDR_MENU1);
 	wc.lpszClassName = _T("MyMainWndClass");
-	if (!RegisterClassEx(&wc))
+	ATOM registeredClass;
+	if (!(registeredClass=RegisterClassEx(&wc)))
 	{
 		DWORD err = GetLastError();
 		return 0;
 	}
 
-	hWndMain = CreateWindow(_T("MyMainWndClass"), _T("MyWindow"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, (HWND)NULL, (HMENU)NULL, hInst, (LPVOID)NULL);
+	hWndMain = CreateWindow(MAKEINTATOM(registeredClass), _T("MyWindow"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, (HWND)NULL, (HMENU)NULL, hInst, (LPVOID)NULL);
 	ShowWindow(hWndMain, SW_SHOWNORMAL);
 	UpdateWindow(hWndMain);
 	MSG msg;
--- MyFirstWin32Demo/resource.h
+++ MyFirstWin32Demo/resource.h
@@ -3,18 +3,23 @@
 // Used by Resource.rc
 //
 #define IDR_MENU1                       101
+#define IDD_FORMVIEW                    101
+#define IDC_EDIT1                       1001
+#define IDC_BUTTON1                     1002
+#define IDC_BUTTON2                     1003
 #define ID_FILE_NEW                     40001
 #define ID_FILE_OPEN                    40002
 #define ID_FILE_SAVE                    40003
 #define ID_FILE_EXIT                    40004
+#define ID_HELP_ABOUT                   40005
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40005
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40006
+#define _APS_NEXT_CONTROL_VALUE         1004
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
